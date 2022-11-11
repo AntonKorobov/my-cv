fd74b1c2931caf9b29db0bd8652bc8cf2758d459
NewAnton <anton1korobov1@gmail.com>
2022-11-11 15:31:57 +0300

    feat: width 320-1024px responsive design

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,40 +1,37 @@
+import './Projects.scss';
 import React from 'react';
 import { ProjectCard } from 'components/ProjectCard/ProjectCard';
 
 export function Projects() {
   return (
     <section className="projects-section section">
-      <div className="projects-section__title title">
-        <span className="title__start-symbol">&#47;&#47;</span>
-        <h2 className="title__h2 h2">Projects</h2>
-      </div>
-      <div className="projects-section__cards-container">
-        <ProjectCard
-          title="RS Lang"
-          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-          img="assets/image-not-found.jpg"
-        />
-      </div>
-      <div className="projects-section__cards-container">
-        <ProjectCard
-          title="RS Lang"
-          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-          img="assets/image-not-found.jpg"
-        />
-      </div>
-      <div className="projects-section__cards-container">
-        <ProjectCard
-          title="RS Lang"
-          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-          img="assets/image-not-found.jpg"
-        />
-      </div>
-      <div className="projects-section__cards-container">
-        <ProjectCard
-          title="RS Lang"
-          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-          img="assets/image-not-found.jpg"
-        />
+      <div className="projects-section__content content-block">
+        <div className="projects-section__title title">
+          <span className="title__start-symbol">&#47;&#47;</span>
+          <h2 className="title__h2 h2">Projects</h2>
+        </div>
+        <div className="projects-section__projects-wrapper">
+          <ProjectCard
+            title="RS Lang"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+            img="assets/image-not-found.jpg"
+          />
+          <ProjectCard
+            title="RS Lang"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+            img="assets/image-not-found.jpg"
+          />
+          <ProjectCard
+            title="RS Lang"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+            img="assets/image-not-found.jpg"
+          />
+          <ProjectCard
+            title="RS Lang"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+            img="assets/image-not-found.jpg"
+          />
+        </div>
       </div>
     </section>
   );
